bad01ae5743ad2ac289365de6bad22a4369e2d51
Kevin Wright <kev.lee.wright@googlemail.com>
2010-06-09 09:26:08 +0100

    Added tests

---
+++ core/src/test/scala/lsug/scaladojo/minesweeper/GridSpec.scala
@@ -0,0 +1,11 @@
+package lsug.scaladojo.minesweeper
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kevin
+ * Date: Jun 8, 2010
+ * Time: 8:58:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class GridSpec
---
+++ core/src/test/scala/lsug/scaladojo/minesweeper/TestGrids.scala
@@ -0,0 +1,11 @@
+package lsug.scaladojo.minesweeper
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kevin
+ * Date: Jun 8, 2010
+ * Time: 10:51:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class TestGrids
